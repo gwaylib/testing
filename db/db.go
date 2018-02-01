f19b591b261534969d276dda62e3752ac8c4414c
shu <free1139@163.com>
2018-02-01 09:58:30 +0800

    rebuild cache

--- db/db.go
+++ db/db.go
@@ -7,9 +7,6 @@ package db
 import (
 	"database/sql"
 	"sync"
-
-	"github.com/gwaylib/datastore/conf/etc"
-	"github.com/gwaylib/errors"
 )
 
 // 仅继承并重写sql.DB, 不增加新的方法，
@@ -32,31 +29,4 @@ func (db *DB) Close() error {
 
 	db.isClose = true
 	return db.DB.Close()
-}
-
-var cache = sync.Map{}
-
-func getDB(etcFileName, sectionName string) (*DB, error) {
-	idb, ok := cache.Load(etcFileName + sectionName)
-	if ok {
-		db := idb.(*DB)
-		if db != nil && !db.isClose {
-			return db, nil
-		}
-	}
-
-	// open
-	cfg, err := etc.GetEtc(etcFileName)
-	if err != nil {
-		return nil, errors.As(err, etcFileName)
-	}
-	drvName := cfg.Section(sectionName).Key("driver").String()
-	dsn := cfg.Section(secName).Key("dsn").String()
-	odb, err := sql.Open(drvName, dsn)
-	if err != nil {
-		return nil, errors.As(err)
-	}
-	db := &DB{DB: odb, isClose: false}
-	cache.Store(etcFileName+sectionName, db)
-	return db, nil
 }
