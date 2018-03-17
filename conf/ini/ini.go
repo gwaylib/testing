ef57776b0a12f077307b5b4d29bf97070686b39e
zhoushuyue <zhoushyue@codein.tv>
2018-03-17 17:02:29 +0800

    fix ini

--- conf/ini/ini.go
+++ conf/ini/ini.go
@@ -8,24 +8,6 @@ import (
 	"github.com/gwaylib/errors"
 )
 
-var cache = sync.Map{}
-
-func GetFile(fileName string) (*ini.File, error) {
-	f, ok := cache.Load(fileName)
-	if ok {
-		return f.(*ini.File), nil
-	}
-	file, err := ini.Load(fileName)
-	if err != nil {
-		if strings.Index(err.Error(), "no such file or directory") > -1 {
-			return nil, errors.ErrNoData.As(err, fileName)
-		}
-		return nil, err
-	}
-	cache.Store(fileName, file)
-	return file, nil
-}
-
 // 用于省略前缀长路径写法
 // 例如,以下可用于多语言处理：
 // ini := NewIni(ini.RootDir()+"/app.default)
@@ -36,11 +18,66 @@ type Ini struct {
 	rootPath string
 }
 
+type File struct {
+	*ini.File
+}
+
+func (f *File) String(section, key string) string {
+	return f.Section(section).Key(key).String()
+}
+func (f *File) Float64(section, key string) float64 {
+	result, err := f.Section(section).Key(key).Float64()
+	if err != nil {
+		panic(err)
+	}
+	return result
+}
+func (f *File) Int64(section, key string) int64 {
+	result, err := f.Section(section).Key(key).Int64()
+	if err != nil {
+		panic(err)
+	}
+	return result
+}
+func (f *File) Uint64(section, key string) uint64 {
+	result, err := f.Section(section).Key(key).Uint64()
+	if err != nil {
+		panic(err)
+	}
+	return result
+}
+func (f *File) Bool(section, key string) bool {
+	result, err := f.Section(section).Key(key).Bool()
+	if err != nil {
+		panic(err)
+	}
+	return result
+}
+
+var cache = sync.Map{}
+
+func GetFile(fileName string) (*File, error) {
+	f, ok := cache.Load(fileName)
+	if ok {
+		return f.(*File), nil
+	}
+	file, err := ini.Load(fileName)
+	if err != nil {
+		if strings.Index(err.Error(), "no such file or directory") > -1 {
+			return nil, errors.ErrNoData.As(err, fileName)
+		}
+		return nil, err
+	}
+	ff := &File{file}
+	cache.Store(fileName, ff)
+	return ff, nil
+}
+
 func NewIni(rootPath string) *Ini {
 	return &Ini{rootPath}
 }
 
-func (ini *Ini) Get(fileName string) *ini.File {
+func (ini *Ini) Get(fileName string) *File {
 	f, err := GetFile(ini.rootPath + fileName)
 	if err != nil {
 		panic(err)
@@ -48,7 +85,7 @@ func (ini *Ini) Get(fileName string) *ini.File {
 	return f
 }
 
-func (ini *Ini) GetDefault(fileName, defFileName string) *ini.File {
+func (ini *Ini) GetDefault(fileName, defFileName string) *File {
 	f, err := GetFile(ini.rootPath + fileName)
 	if err != nil {
 		if !errors.ErrNoData.Equal(err) {
